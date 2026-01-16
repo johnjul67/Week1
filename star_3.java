873cb2d84e2d28c49827d48b58dd56ffa83a86e4
John <johnjulious2006@gmail.com>
2026-01-16 09:58:58 +0530

    Update star_3.java

--- star_3.java
+++ star_3.java
@@ -1,12 +1,16 @@
-public class star_3
-{
-    public static void main()
+public class Triangle {
+    public static void main(String[] args)
     {
 		int c=1;
         for(int i=0;i<3;i++)
 		{
-			for(int j=0;j<c;j++)
-				System.out.print("*");
+			for (int j = 0; j < c; j++) 
+            {
+            if (j == c - 1)
+                System.out.print("*");
+            else
+                System.out.print("* ");
+            }
 			System.out.println();
 			c+=2;
 		}      
